766b96a443f4575c9b7a27affa5e1d255927a67d
Perkles <gabrielle.leal773@gmail.com>
2022-07-13 23:30:28 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,45 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World!! " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String none;
+    private Integer numPaginas;
+
+    public Livro(String none, Integer numPaginas) {
+        this.none = none;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNone() {
+        return none;
+    }
+
+    public void setNone(String none) {
+        this.none = none;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "none='" + none + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
